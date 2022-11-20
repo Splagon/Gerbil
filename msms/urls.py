0c94d4028595b6d467b64c10831e6df9cb58f185
Wesley Lin <wesley.lin@kcl.ac.uk>
2022-11-20 11:41:15 +0000

    - Renamed tests.py to test_user_model.py for clarity - Added sign_up.html and corresponding view - Added sign_up to urls

--- msms/urls.py
+++ msms/urls.py
@@ -19,5 +19,6 @@ from lessons import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name = 'home')
+    path('', views.home, name = 'home'),
+    path("sign_up/", views.sign_up, name="sign_up"),
 ]
