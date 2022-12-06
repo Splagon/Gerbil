0a210b52afa418e422e962de9663abe2fd05b94d
Manik Aggarwal <manik.aggarwal@kcl.ac.uk>
2022-12-06 22:39:22 +0000

    Migrations work

--- lessons/migrations/0001_initial.py
+++ lessons/migrations/0001_initial.py
@@ -22,22 +22,34 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='User',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('password', models.CharField(max_length=128, verbose_name='password')),
-                ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),
-                ('email', models.EmailField(blank=True, max_length=254, verbose_name='email address')),
-                ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
-                ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
-                ('username', models.EmailField(max_length=254, unique=True, validators=[django.core.validators.EmailValidator(message='Invalid email')], verbose_name='email')),
+                ('id', models.BigAutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
+                ('password', models.CharField(
+                    max_length=128, verbose_name='password')),
+                ('last_login', models.DateTimeField(
+                    blank=True, null=True, verbose_name='last login')),
+                ('email', models.EmailField(blank=True,
+                 max_length=254, verbose_name='email address')),
+                ('is_active', models.BooleanField(
+                    default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
+                ('date_joined', models.DateTimeField(
+                    default=django.utils.timezone.now, verbose_name='date joined')),
+                ('username', models.EmailField(max_length=254, unique=True, validators=[
+                 django.core.validators.EmailValidator(message='Invalid email')], verbose_name='email')),
                 ('first_name', models.CharField(max_length=50)),
                 ('last_name', models.CharField(max_length=50)),
-                ('dateOfBirth', models.DateField(blank=True, max_length=8, null=True, verbose_name='Date of Birth')),
+                ('dateOfBirth', models.DateField(blank=True,
+                 max_length=8, null=True, verbose_name='Date of Birth')),
                 ('is_staff', models.BooleanField(verbose_name='Admin Status')),
-                ('is_superuser', models.BooleanField(verbose_name='Director Status')),
-                ('is_adult', models.BooleanField(default=False, verbose_name='Adult Status')),
+                ('is_superuser', models.BooleanField(
+                    verbose_name='Director Status')),
+                ('is_adult', models.BooleanField(
+                    default=False, verbose_name='Adult Status')),
                 ('balance', models.FloatField(default=0.0)),
-                ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups')),
-                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.permission', verbose_name='user permissions')),
+                ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+                 related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups')),
+                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.',
+                 related_name='user_set', related_query_name='user', to='auth.permission', verbose_name='user permissions')),
             ],
             options={
                 'verbose_name': 'user',
@@ -51,7 +63,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Invoice',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
                 ('unique_reference_number', models.CharField(max_length=100)),
                 ('invoice_number', models.CharField(max_length=36)),
                 ('student_id', models.IntegerField(default=0)),
@@ -63,23 +76,28 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='SchoolBankAccount',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
                 ('balance', models.FloatField(default=0.0)),
             ],
         ),
         migrations.CreateModel(
             name='Term',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
                 ('termName', models.CharField(default='blank', max_length=50)),
-                ('startDate', models.DateField(default=datetime.date.today, unique=True)),
-                ('endDate', models.DateField(default=datetime.date.today, unique=True)),
+                ('startDate', models.DateField(
+                    default=datetime.date.today, unique=True)),
+                ('endDate', models.DateField(
+                    default=datetime.date.today, unique=True)),
             ],
         ),
         migrations.CreateModel(
             name='Adult',
             fields=[
-                ('user_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL)),
+                ('user_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE,
+                 parent_link=True, primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'verbose_name': 'Adult',
@@ -92,35 +110,45 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Request',
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
-                ('availability_date', models.DateField(default=django.utils.timezone.now)),
+                ('id', models.UUIDField(default=uuid.uuid4,
+                 editable=False, primary_key=True, serialize=False)),
+                ('availability_date', models.DateField(
+                    default=django.utils.timezone.now)),
                 ('availability_time', models.TimeField(default='08:00')),
-                ('interval_between_lessons', models.CharField(choices=[('1', 1), ('2', 2)], max_length=2)),
-                ('duration_of_lessons', models.CharField(choices=[('30', '30'), ('45', '45'), ('60', '60')], max_length=4)),
-                ('instrument', models.CharField(blank=True, choices=[('violin', 'violin'), ('double bass', 'double bass'), ('cello', 'cello')], max_length=180)),
+                ('interval_between_lessons', models.CharField(
+                    choices=[('1', 1), ('2', 2)], max_length=2)),
+                ('duration_of_lessons', models.CharField(choices=[
+                 ('30', '30'), ('45', '45'), ('60', '60')], max_length=4)),
+                ('instrument', models.CharField(blank=True, choices=[
+                 ('violin', 'violin'), ('double bass', 'double bass'), ('cello', 'cello')], max_length=180)),
                 ('teacher', models.CharField(blank=True, max_length=50)),
                 ('status', models.CharField(default='In Progress', max_length=50)),
                 ('totalPrice', models.CharField(default=0, max_length=50)),
                 ('requesterId', models.IntegerField(default=0)),
-                ('username', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('username', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.CreateModel(
             name='BankTransfer',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
                 ('invoice_number', models.CharField(max_length=50)),
                 ('amount', models.FloatField(default=0)),
                 ('student_id', models.IntegerField(default=0)),
-                ('username', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('username', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.CreateModel(
             name='AdultChildRelationship',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
                 ('child', models.CharField(max_length=50)),
-                ('adult', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='lessons.adult')),
+                ('adult', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to='lessons.adult')),
             ],
         ),
     ]
