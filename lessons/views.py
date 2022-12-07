b223034ae9f5a1c0244f863e28f11872233fad6a
Wesley Lin <wesley.lin@kcl.ac.uk>
2022-12-07 14:44:13 +0000

    - fix errors

--- lessons/views.py
+++ lessons/views.py
@@ -49,8 +49,9 @@ def view_child(request):
 @login_required(login_url="log_in")
 def delete_child(request, child_id):
     user = request.user
-    adult_ = Adult.objects.get(username=user.username)
-    relation = AdultChildRelationship.objects.get(adult=adult_id, child=child_id)
+    adult_id = Adult.objects.get(username=user.username)
+    the_child = User.objects.get(id=child_id)
+    relation = AdultChildRelationship.objects.get(adult=adult_id, child=the_child)
     relation.delete()
     return redirect('view_child')
 
