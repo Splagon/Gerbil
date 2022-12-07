ca1fc141c5919cc54a65aec37b2640cfb802545b
Manik Aggarwal <manik.aggarwal@kcl.ac.uk>
2022-12-07 18:19:03 +0000

    Default children of John Doe

--- lessons/management/commands/seed.py
+++ lessons/management/commands/seed.py
@@ -1,16 +1,17 @@
 from django.core.management.base import BaseCommand, CommandError
 from faker import Faker
-from lessons.models import Adult, User, Request
+from lessons.models import Adult, User, Request, AdultChildRelationship
 from lessons.models import Term
 from django.db.utils import IntegrityError
 import datetime
 from random import randint, random
-from lessons.helpers import getDurations, getInstruments, getIntervalBetweenLessons
+from lessons.helpers import getDurations, getInstruments
 
 
 class Command(BaseCommand):
     PASSWORD = "Password123"
-    USER_COUNT = 100
+    ADULT_USER_COUNT = 100
+    CHILDREN_USER_COUNT = 100
     REQUEST_COUNT = 100
     JOHNDOE_REQUEST_COUNT = 5
     NUMBER_OF_TERMS = 6
@@ -39,8 +40,10 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.create_terms()
-        self.create_all_users()
-        self.allUsers = User.objects.all()
+        self.create_all_adult_users()
+        self.allAdultUsers = User.objects.filter(is_adult=True)
+        self.create_all_children_users()
+        self.allChildUsers = User.objects.filter(is_adult = False)
         self.create_requests()
 
     def create_terms(self):
@@ -56,7 +59,7 @@ class Command(BaseCommand):
             print(f'Seeding term {i+1}',  end='\r')
         print('Term seeding complete')
 
-    def create_all_users(self):
+    def create_all_adult_users(self):
         self.create_predefined_users()
         self.create_all_adult_users()
 
@@ -72,7 +75,7 @@ class Command(BaseCommand):
     def create_all_adult_users(self):
         user_count = 0
         # creates all the random users
-        while user_count < Command.USER_COUNT:
+        while user_count < Command.ADULT_USER_COUNT:
             print(f'Seeding user {user_count}',  end='\r')
             first_name = self.faker.first_name()
             last_name = self.faker.last_name()
@@ -96,18 +99,50 @@ class Command(BaseCommand):
             is_superuser=isDirector
         )
 
-    # def create_user(self):
-    #     first_name = self.faker.first_name()
-    #     last_name = self.faker.last_name()
-    #     username = self._username(first_name, last_name)
-    #     dateOfBirth = self.faker.date_of_birth(None, 18, 60)
-    #     Adult.objects.create_user(
-    #         username=username,
-    #         first_name=first_name,
-    #         last_name=last_name,
-    #         dateOfBirth=dateOfBirth,
-    #         password=Command.PASSWORD
-    #     )
+    def create_all_children_users(self):
+        self.create_predefined_children()
+        self.create_random_children()
+        
+    def create_predefined_children(self):
+        try:
+            self.create_children_user(
+                "alice.doe@example.org", "Alice", "Doe", "john.doe@example.org")
+            self.create_children_user(
+                "bob.doe@example.org", "Bob", "Doe", "john.doe@example.org")
+        except (IntegrityError):
+            pass
+
+    def create_random_children(self):
+        user_count = 0
+        # creates all the random users
+        while user_count < Command.CHILDREN_USER_COUNT:
+            print(f'Seeding children {user_count}',  end='\r')
+            first_name = self.faker.first_name()
+            last_name = self.faker.last_name()
+            username = self._username(first_name, last_name)
+            parent_username = self.get_random_adult_user()
+            try:
+                self.create_children_user(username, first_name, last_name, parent_username)
+            except (IntegrityError):
+                continue
+            user_count += 1
+        print('Children user seeding complete')
+
+    def create_children_user(self, username, first_name, last_name, parent_username):
+        User.objects.create_user(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            # Has to be under 18 as they are a child
+            dateOfBirth=self.faker.date_of_birth(None, 5, 18),
+            password=Command.PASSWORD,
+            is_adult=False
+        )
+        # Creates the corresponding parent realtionship for the child
+        relationship = AdultChildRelationship()
+        relationship.adult = Adult.objects.get(username=parent_username)
+        relationship.child = User.objects.get(username=username)
+        relationship.save()
 
     def _username(self, first_name, last_name):
         username = f'{first_name}.{last_name}@example.org'
@@ -115,16 +150,19 @@ class Command(BaseCommand):
 
     def create_requests(self):
         # makes requests for John Doe
-        john_doe = self.get_user("john.doe@example.org")
+        john_doe = User.objects.get(username="john.doe@example.org")
         for i in range(Command.JOHNDOE_REQUEST_COUNT):
             self.create_a_request(john_doe)
-
+        # Makes the requests for other users
         request_count = 0
         while request_count < Command.REQUEST_COUNT:
             print(f'Seeding request {request_count}',  end='\r')
-            self.create_a_request(self.get_random_user())
+            self.create_a_request(self.get_random_adult_user())
             request_count += 1
         print("Request seeding complete")
+
+    def choose_which_user_request(self):
+        pass
 
     def create_a_request(self, user):
         request = Request()
@@ -141,14 +179,14 @@ class Command(BaseCommand):
         request.status = self.get_random_status()
         request.save()
 
-    def get_user(self, username):
-        for user in self.allUsers:
-            if user.username == username:
-                return user
+    # def get_user(self, username):
+    #     for user in self.allAdultUsers:
+    #         if user.username == username:
+    #             return user
 
-    def get_random_user(self):
-        index = randint(0, self.allUsers.count()-1)
-        return self.allUsers[index]
+    def get_random_adult_user(self):
+        index = randint(0, self.allAdultUsers.count()-1)
+        return self.allAdultUsers[index]
 
     def get_random_term_date(self):
         while (True):
@@ -161,56 +199,3 @@ class Command(BaseCommand):
 
     def get_random_status(self):
         return "Booked" if Command.REQUEST_FULFILL_PROBABILITY > random() else "In Progress"
-
-    # def create_johndoe_student(self):
-    #     Adult.objects.create_user(
-    #         username="john.doe@example.org",
-    #         first_name="John",
-    #         last_name="Doe",
-    #         dateOfBirth=self.faker.date_of_birth(None, 18, 60),
-    #         password=Command.PASSWORD
-    #     )
-
-    def create_alicedoe_child(self):
-        User.objects.create_user(
-            username="alice.doe@example.org",
-            first_name="Alice",
-            last_name="Doe",
-            # Has to be under 18 as they are a child
-            dateOfBirth=self.faker.date_of_birth(None, 5, 18),
-            password=Command.PASSWORD,
-            is_adult = False
-        )
-
-    def create_bobdoe_child(self):
-        User.objects.create_user(
-            username="bob.doe@example.org",
-            first_name="Bob",
-            last_name="Doe",
-            # Has to be under 18 as they are a child
-            dateOfBirth=self.faker.date_of_birth(None, 5, 18),
-            password=Command.PASSWORD,
-            is_adult=False
-        )
-
-    # def create_petrapickles_admin(self):
-    #     Adult.objects.create_user(
-    #         username="petra.pickles@example.org",
-    #         first_name="Petra",
-    #         last_name="Pickles",
-    #         dateOfBirth=self.faker.date_of_birth(None, 18, 60),
-    #         password=Command.PASSWORD,
-    #         is_staff=True,
-    #         is_superuser=False
-    #     )
-
-    # def create_martymajor_director(self):
-    #     Adult.objects.create_user(
-    #         username="marty.major@example.org",
-    #         first_name="Marty",
-    #         last_name="Major",
-    #         dateOfBirth=self.faker.date_of_birth(None, 18, 60),
-    #         password=Command.PASSWORD,
-    #         is_staff=True,
-    #         is_superuser=True
-    #     )
